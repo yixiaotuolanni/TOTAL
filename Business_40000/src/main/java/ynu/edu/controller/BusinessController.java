c85561b401aa70cfed58ae1c753c60268676ce12
谢庚言 <835456664@qq.com>
2024-06-17 23:18:54 +0800

    增加消息总线和根据config改变服务质量

--- Business_40000/src/main/java/ynu/edu/controller/BusinessController.java
+++ Business_40000/src/main/java/ynu/edu/controller/BusinessController.java
@@ -1,25 +1,33 @@
 package ynu.edu.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 import ynu.edu.feign.BusinessFeignClient;
 import ynu.edu.pojo.Business;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @CrossOrigin
 @RequestMapping(value = "/business")
 public class BusinessController {
     @Resource
     BusinessFeignClient businessFeignClient;
+    @Value("${protectSignal}")
+    private Boolean protectSignal;
     @RequestMapping("/test")
     public String test(){
         return "接口可以正常调用";
     }
+
     @GetMapping("/listBusiness")
     public List<Business> listBusiness() throws InterruptedException {
-        Thread.sleep(4_000L);
+        if (protectSignal){
+            Thread.sleep(4_000L);
+        }
         return businessFeignClient.listBusiness();
     }
     @GetMapping("/listBusinessByOrderTypeId")
